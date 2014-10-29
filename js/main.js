7ae3ba2f44d2faac485a6b247e8f6e9120bd5930
Lucas <lucas@agtlucas.com>
2014-10-29 10:11:07 -0200

    Adding js folder

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var app = (function (window, document, undefined) {
+
+  var app = {
+
+    init: function () {
+      this.alerter();
+    },
+
+    alerter: function () {
+      alert('WOW');
+    }
+
+  };
+
+  return app.init();
+
+})(window, document);
