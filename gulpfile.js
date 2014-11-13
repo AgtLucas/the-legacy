0018e8cea4972838ac2b3fda221e904b867b80f2
Lucas <lucas@agtlucas.com>
2014-11-13 00:09:42 -0200

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('browserify', function () {
 
 // Sass Task
 gulp.task('sass', function () {
-  gulp.src('_src/_sass/main.scss')
+  gulp.src('src/_sass/main.scss')
     .pipe(sass({style: 'expanded'}))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('css'))
