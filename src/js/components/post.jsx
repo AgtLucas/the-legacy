08e10e6989a251b94613f38fe9a1e0124b2013f8
Lucas <lucas@agtlucas.com>
2014-11-27 10:10:04 -0200

    Update post.jsx

--- src/js/components/post.jsx
+++ src/js/components/post.jsx
@@ -1,7 +1,18 @@
 var React = require("react");
 
 var Post = React.createClass({
+  getInitialState: function () {
 
+  },
+  componentsWillMount: function () {
+
+  },
+  _onChange: function () {
+
+  },
+  render: function () {
+
+  }
 });
 
 module.exports = Post;
