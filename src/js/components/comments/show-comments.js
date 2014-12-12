89f16352b3da10c88d80a6760d878a33127b5107
Lucas <lucas@agtlucas.com>
2014-12-12 09:09:05 -0200

    Update show comments

---
+++ src/js/components/comments/show-comments.js
@@ -0,0 +1,14 @@
+var loadDisqus = require('./load-disqus');
+
+var showComments = function () {
+  var showComments = document.getElementById('show-comments');
+
+  if (showComments) {
+    showComments.addEventListener('click', function () {
+      showComments.parentNode.removeChild(showComments);
+      loadDisqus();
+    });
+  }
+};
+
+module.exports = showComments;
