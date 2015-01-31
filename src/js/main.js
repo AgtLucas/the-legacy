c1ba5fa3eec89f76ec351e42ca87a9294abb7c16
Lucas <lucas@agtlucas.com>
2015-01-31 16:59:02 -0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+var rwdVideos = require('./components/videos/responsive-video');
 var loadComments = require('./components/comments/show-comments');
 
+rwdVideos.rwdVideos();
 loadComments.showComments();
