e1196f46086920ab068169b355519cc0a5fbc75a
Lucas <lucas@agtlucas.com>
2015-01-31 17:45:03 -0200

    Delete disqus

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
 var rwdVideos = require('./components/videos/responsive-video');
-var loadComments = require('./components/comments/show-comments');
 
 rwdVideos.rwdVideos();
-loadComments.showComments();
