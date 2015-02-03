b84da1091c64d8684a5a1e699b243dd6611250d5
Lucas <lucas@agtlucas.com>
2015-02-02 22:29:49 -0200

    Updates

---
+++ src/js/components/videos/responsive-video.js
@@ -0,0 +1,12 @@
+var fluidvids = require('fluidvids.js/dist/fluidvids')();
+
+var rwdVideos = function () {
+  fluidvids.init({
+    selector: ['iframe', 'object'],
+    players: ['www.youtube.com', 'player.vimeo.com']
+  });
+};
+
+module.exports = {
+  rwdVideos: rwdVideos
+};
